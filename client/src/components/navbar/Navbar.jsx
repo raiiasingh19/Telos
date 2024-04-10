744fb51c590f63f1a4d3950ff9ac85827bfdf3ef
raiiasingh19 <f20210278@goa.bits-pilani.ac.in>
2024-04-11 00:32:45 +0530

    pogo

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Form from "../form/Form.jsx";
 // import Profile from "../profile/Profile.jsx";
@@ -6,11 +6,56 @@ import '../../App.css';
 
 export default function Navbar() {
     const [isUploadClicked, setIsUploadClicked] = useState(false)
+    
+  
+        const [keyPressed, setKeyPressed] = useState('');
+        const [lastFourChars, setlastFourChars] = useState('');
+        console.log(keyPressed)
+
+        const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
+
+        useEffect(() => {
+            const handleKeyDown = (event) => {
+                setKeyPressed(keyPressed+event.key);
+                if (event.key === 'Enter') {
+                  setlastFourChars(keyPressed.slice(-4));
+                  setIsAdminLoggedIn(true);
+                   
+                }
+                
+            };
+            
+            const handleEnterKey = (event) => {
+              if(event.key === 'Enter' && isAdminLoggedIn) {
+                setKeyPressed(''); // Clear keystrokes after detecting "pogo"
+              }
+              
+            };
+
+            window.addEventListener('keydown', handleKeyDown);        
+            window.addEventListener('keydown', handleEnterKey);
+        
+            return () => {
+              window.removeEventListener('keydown', handleKeyDown);
+              window.removeEventListener('keydown', handleEnterKey);
+            };
+          
+
+        }, [isAdminLoggedIn, lastFourChars,keyPressed])
+        console.log(isAdminLoggedIn)
+        console.log("lfc: ", lastFourChars)
+
+        
+    
+
+    
 
     const ToggleSidebar = () => {
         setIsUploadClicked(prevState => !prevState);
     };
 
+    
+    
     return (
         <nav className="navbar">
             <span className="logo">TELOS ART</span>
@@ -18,10 +63,14 @@ export default function Navbar() {
             <Link to="/gallery" className="nav-link"><span className="nav-text">GALLERY</span></Link>
             <Link to="/contact" className="nav-link"><span className="nav-text">CONTACT</span></Link>
             {/* <span className="nav-text"><Profile handleLogout={handleLogout} user={user} /></span> */}
-            {/* {user?.email === "f20210274@goa.bits-pilani.ac.in" ? (
-                <span className="nav-text" id="upload" onClick={ToggleSidebar}>+</span>
-            ) : null} VERY IMP */}
+
+            
+          {isAdminLoggedIn ? (<span className="nav-text" id="upload" onClick={ToggleSidebar}>+</span>) : null}
+        
+
+
             <Form isUploadClicked={isUploadClicked} setIsUploadClicked={setIsUploadClicked} ToggleSidebar={ToggleSidebar} />
         </nav>
     )
 }
+
